aa0448117d0500325ad5efa0f912c1410a369b12
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-08-25 10:21:14 +0000

    Se añade tabla Class y endpoint correspondientes, además, se añade botón para volver a logín en recuperación de password

--- src/front/js/component/sidebar/sidebar.js
+++ src/front/js/component/sidebar/sidebar.js
@@ -1,140 +1,134 @@
-import React, { useState, useContext } from "react";
-import { Link } from "react-router-dom";
-import { Context } from "../../store/appContext";
-import { useLocation } from 'react-router-dom';
+    import React, { useState, useContext } from "react";
+    import { Link } from "react-router-dom";
+    import { Context } from "../../store/appContext";
+    import { useLocation } from 'react-router-dom';
 
 
-export const Sidebar = () => {
-    // console.log(useLocation().pathname);
+    export const Sidebar = () => {
+        
 
-    const { store, actions } = useContext(Context);
-    const [showSidebar, setShowSidebar] = useState("spread")
+        const { store, actions } = useContext(Context);
+        const [showSidebar, setShowSidebar] = useState("spread")
 
 
-    const handleToggleSideBar = () => {
-        if (Sidebar !=null){
+        const handleToggleSideBar = () => {
+            if (Sidebar !=null){
 
-            if (showSidebar === "spread") { setShowSidebar("shrink") }
-            else { setShowSidebar("spread") }
+                if (showSidebar === "spread") { setShowSidebar("shrink") }
+                else { setShowSidebar("spread") }
+            }
         }
-    }
-    }
+        
 
-
-    if (store.logged === false || (useLocation().pathname === "/") ) return null
-    else {
-
-        if (showSidebar === "spread") {
-
-            return (
-                <>
-
-                    < nav className="sidebar mt-5 align-items-center my-auto py-4 pe-5 ps-3 ">
-
-                        <ul className="list-unstyled components mb-0 pb-0 lista">
-                            <li>
-                                <i className=" arrow-left fa-solid fa-arrow-left ms-3 me-3  mb-3 d-flex justify-content-end" onClick={handleToggleSideBar}></i>
-
-                            </li>
-                            <Link to="/dashboard" className="link-logo">
-                                <li className="pt-0 dashboard">
-                                    <i className="fa-solid fa-table-cells-large m-3 "></i>
-                                    <a className="">Dashboard</a>
-                                </li>
-                            </Link>
-                            <Link to="/students" className="link-logo">
-                                <li className="students">
-                                    <i className="fa-solid fa-graduation-cap m-3"></i>
-                                    <a className="">Students</a>
-                                </li>
-                            </Link>
-                            <Link to="/subjects" className="link-logo">
-                                <li className="subjects">
-                                    <i className="fa-solid fa-book m-3"></i>
-                                    <a className="">Subjects</a>
-                                </li>
-                            </Link>
-                            <Link to="/profile" className="link-logo">
-                                <li className="profile">
-                                    <i className="fa-solid fa-user m-3"></i>
-                                    <a className="">Profile</a>
-                                </li>
-                            </Link>
-
-                            <Link to="/settings" className="link-logo">
-                                <li className="settings">
-                                    <i className="fa-solid fa-gear m-3 "></i>
-                                    <a className="">Settings</a>
-                                </li>
-                            </Link>
-                            <Link to="/documents" className="link-logo">
-                                <li className="documents">
-                                    <i className="fa-regular fa-folder-open m-3"></i>
-                                    <a className="">Documents</a>
-                                </li>
-                            </Link>
-                        </ul>
-
-                    </nav >
-
-
-                </>
-
-            );
-        }
-
+        if (store.logged === false || (useLocation().pathname === "/") ) return null
         else {
-            return (
-                <>
 
-                    < nav className="sidebar mt-5 align-items-center my-auto py-4 pe-2 ps-3 ">
+            if (showSidebar === "spread") {
 
-                        <ul className="list-unstyled components mb-0 pb-0">
-                            <li>
-                                <i className="arrow-right fa-solid fa-arrow-right ms-3 me-3  mb-3" onClick={handleToggleSideBar}></i>
+                return (
+                    <>
 
-                            </li>
-                            <Link to="/dashboard" className="link-logo">
-                                <li className="pt-0 dashboard">
-                                    <i className="fa-solid fa-table-cells-large m-3 "></i>
+                        < nav className="sidebar mt-5 align-items-center my-auto py-4 pe-5 ps-3 ">
+
+                            <ul className="list-unstyled components mb-0 pb-0 lista">
+                                <li>
+                                    <i className=" arrow-left fa-solid fa-arrow-left ms-3 me-3  mb-3 d-flex justify-content-end" onClick={handleToggleSideBar}></i>
+
                                 </li>
-                            </Link>
-                            <Link to="/students" className="link-logo">
-                                <li className="students">
-                                    <i className="fa-solid fa-graduation-cap m-3"></i>
-                                </li>
-                            </Link>
-                            <Link to="/subjects" className="link-logo">
-                                <li className="subjects">
-                                    <i className="fa-solid fa-book m-3"></i>
-                                </li>
-                            </Link>
-                            <Link to="/profile" className="link-logo">
-                                <li className="profile">
-                                    <i className="fa-solid fa-user m-3"></i>
-                                </li>
-                            </Link>
-                            <Link to="/settings" className="link-logo">
-                                <li className="settings">
-                                    <i className="fa-solid fa-gear m-3 "></i>
-                                </li>
-                            </Link>
-                            <Link to="/documents" className="link-logo">
-                                <li className="documents">
-                                    <i className="fa-regular fa-folder-open m-3"></i>
-                                </li>
-                            </Link>
-                        </ul>
+                                <Link to="/dashboard" className="link-logo">
+                                    <li className="pt-0 dashboard">
+                                        <i className="fa-solid fa-table-cells-large m-3 "></i>
+                                        <a className="">Dashboard</a>
+                                    </li>
+                                </Link>
+                                <Link to="/students" className="link-logo">
+                                    <li className="students">
+                                        <i className="fa-solid fa-graduation-cap m-3"></i>
+                                        <a className="">Students</a>
+                                    </li>
+                                </Link>
+                                <Link to="/subjects" className="link-logo">
+                                    <li className="subjects">
+                                        <i className="fa-solid fa-book m-3"></i>
+                                        <a className="">Subjects</a>
+                                    </li>
+                                </Link>
+                                <Link to="/profile" className="link-logo">
+                                    <li className="profile">
+                                        <i className="fa-solid fa-user m-3"></i>
+                                        <a className="">Profile</a>
+                                    </li>
+                                </Link>
 
-                    </nav >
+                                <Link to="/settings" className="link-logo">
+                                    <li className="settings">
+                                        <i className="fa-solid fa-gear m-3 "></i>
+                                        <a className="">Settings</a>
+                                    </li>
+                                </Link>
+                                <Link to="/documents" className="link-logo">
+                                    <li className="documents">
+                                        <i className="fa-regular fa-folder-open m-3"></i>
+                                        <a className="">Documents</a>
+                                    </li>
+                                </Link>
+                            </ul>
+
+                        </nav >
 
 
-                </>
+                    </>
 
-            );
+                );
+            }
+
+            else {
+                return (
+                    <>
+
+                        < nav className="sidebar mt-5 align-items-center my-auto py-4 pe-2 ps-3 ">
+
+                            <ul className="list-unstyled components mb-0 pb-0">
+                                <li>
+                                    <i className="arrow-right fa-solid fa-arrow-right ms-3 me-3  mb-3" onClick={handleToggleSideBar}></i>
+
+                                </li>
+                                <Link to="/dashboard" className="link-logo">
+                                    <li className="pt-0 dashboard">
+                                        <i className="fa-solid fa-table-cells-large m-3 "></i>
+                                    </li>
+                                </Link>
+                                <Link to="/students" className="link-logo">
+                                    <li className="students">
+                                        <i className="fa-solid fa-graduation-cap m-3"></i>
+                                    </li>
+                                </Link>
+                                <Link to="/subjects" className="link-logo">
+                                    <li className="subjects">
+                                        <i className="fa-solid fa-book m-3"></i>
+                                    </li>
+                                </Link>
+                                <Link to="/profile" className="link-logo">
+                                    <li className="profile">
+                                        <i className="fa-solid fa-user m-3"></i>
+                                    </li>
+                                </Link>
+                                <Link to="/settings" className="link-logo">
+                                    <li className="settings">
+                                        <i className="fa-solid fa-gear m-3 "></i>
+                                    </li>
+                                </Link>
+                                <Link to="/documents" className="link-logo">
+                                    <li className="documents">
+                                        <i className="fa-regular fa-folder-open m-3"></i>
+                                    </li>
+                                </Link>
+                            </ul>
+
+                        </nav >
+
+                    </>
+                );
+            }
         }
-
     }
-
-
-};
