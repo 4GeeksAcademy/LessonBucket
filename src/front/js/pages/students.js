6133fd0910fb3b267a8b71abf8c0178880e39947
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-09-04 11:09:42 +0000

    Se hace migración

--- src/front/js/pages/students.js
+++ src/front/js/pages/students.js
@@ -226,27 +226,6 @@ export const Students = () => {
               ))
             )}
           </div>
-          {/* INICIO PAGINACIÓN */}
-
-          {/* <nav aria-label="Page navigation example">
-              <ul className="pagination">
-                <li className="page-item">
-                  <a className="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li className="page-item"><a className="page-link" href="#">1</a></li>
-                <li className="page-item"><a className="page-link" href="#">2</a></li>
-                <li className="page-item"><a className="page-link" href="#">3</a></li>
-                <li className="page-item">
-                  <a className="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav> */}
-
-          {/* FIN DE PAGINACIÓN */}
         </>
       ) : (
         <div className="recover-pass-main">
