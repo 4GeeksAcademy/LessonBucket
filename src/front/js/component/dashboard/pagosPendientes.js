5145807ec603647c84ebb7c07fa8ea4a987b7295
Miguel Martin <miguelmr94@gmail.com>
2023-09-15 08:51:12 +0000

    Button placed and opens modal

--- src/front/js/component/dashboard/pagosPendientes.js
+++ src/front/js/component/dashboard/pagosPendientes.js
@@ -49,14 +49,34 @@ export const PagosPendientes = (props) => {
 					<div className="overflow-auto">
 						<h5 className="pill-title">Pending payment </h5>
 						{loaded === "fullLoaded" && paymentFiltrados ? paymentFiltrados.map((item, index) => (
-							<div className="pill" key={index}>
-								<p className="pill-font">{item.student?.name}-{item.date}</p>
+							<div className="pill row" key={index}>
+								<p className="pill-font col-9">{item.student?.name}-{item.date}</p>
+								<button type="button" class="btn btn-primary col-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  									Paid
+								</button>
 							</div>
 						)): "No hay pagos pendientes"}
 					</div>
 				</div>
 			</div>
+			
+			<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  			<div class="modal-dialog">
+    				<div class="modal-content">
+      					<div class="modal-header">
+        					<h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      					</div>
+      					<div class="modal-body">
+        				...
+      					</div>
+      					<div class="modal-footer">
+        					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        					<button type="button" class="btn btn-primary">Save changes</button>
+      					</div>
+    				</div>
+  				</div>
+			</div>
 		</div>
-
 	);
 };
